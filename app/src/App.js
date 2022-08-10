300b2293e40f3d920d68aa1f055e9ad3e5fd0c0d
TI - Ortep <ti@ortep.corp>
2022-08-10 15:56:48 -0300

    Commit message

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,10 @@ const App = () => {
   const [keys, setKeys] = React.useState([])
   const [value, setValue] = React.useState([])
   const [selectedOption, setSelectedOption] = React.useState()
+  const [selectedOption2, setSelectedOption2] = React.useState()  
+  const [quantmoney, setquantmoney] = React.useState()
   
+
   const getCurrency = async () => {
     const response = await axios.get("http://localhost:8080/currency")
     const { rates } = response.data
@@ -16,27 +19,36 @@ const App = () => {
     setValue(Object.values(rates))
   }
 
+  
   React.useEffect(() => {
-    console.log("Minha pika")
     getCurrency()
   }, [])
 
-
+ // 1 / preço moeda = preço da moeda selecionada
+ // preço da moeda selecionada * preço da moeda comparada 1 euro = 1
+ // para comparar duas moedas →→→  ?????? ao AED e BRL tem que dar +- 1,39
+ // AED, AED / 1 → 3,737438 , 0,26756296693082265  BRL , BRL / 1 → 5,256675 , 0,19023432112504576
+ // para comparar duas moedas →→→ 1 / preço ao euro * preço ao euro.
   return (
-    <div className="App">
-      <select name="" id="din1" onChange={(e) => setSelectedOption(e.target.value)}>
-        {keys.map((key, index) => (
-          <option key={key} value={value[index]}>{key}</option>
-        ))}
-      </select>
-
-      <input value={selectedOption} readOnly />
-
-      <select name="" id="din2" onChange={(e) => setSelectedOption(e.target.value)}>
-        {keys.map((key, index) => (
-          <option key={key} value={value[index]}>{key}</option>
-        ))}
-      </select>
+    <div className="App-body">
+      <div>
+        <div className='firstselect'>       
+          <select className='App-select' name="" id="din1" onChange={(e) =>  setSelectedOption(1 / e.target.value)}>
+            {keys.map((key, index) => (
+              <option key={key} value={value[index]}>{key}</option>
+            ))}
+          </select>  
+          <input className='App' type="number" onChange={(e) => setquantmoney(e.target.value)} value={quantmoney} />
+        </div>
+        <div>
+          <select className='App-select' name="" id="din2" onChange={(e) =>  setSelectedOption2(e.target.value)}>
+            {keys.map((key, index) => (
+              <option key={key} value={value[index]}>{key}</option>
+            ))}
+          </select>
+          <input className='App' value={(quantmoney * selectedOption) * selectedOption2} readOnly />       
+        </div>
+      </div>
     </div>
   )
 }
